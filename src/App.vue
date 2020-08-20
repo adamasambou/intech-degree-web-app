fa5c67c1caa9fb325a97f27cb03afae14d730ad2
Adama Sambou <info@adamasambou.co.uk>
2020-08-20 14:05:18 +0100

    tessssst

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/Intech-Logo.png">
-    <HelloWorld msg="Welcome to Adama Sambou's final year Project..."/>
+    <HelloWorld msg="Welcome to Adamaaaaa Sambou's final year Project..."/>
   </div>
 </template>
 
