13a34774974dbf47fd6d1ad5457242718b625c07
Adama Sambou <info@adamasambou.co.uk>
2020-08-20 12:55:26 +0100

    just testing commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/Intech-Logo.png">
-    <HelloWorld msg="Welcome to Adama Sambou's final year Project..."/>
+    <HelloWorld msg="Welcome to Adaaamamaama Sambou's final year Project..."/>
   </div>
 </template>
 
@@ -10,7 +10,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
 export default 
 {
-  name: 'Application',
+  name: 'App',
   components: {
     HelloWorld
   }
